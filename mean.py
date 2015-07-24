66cbf19691c14b58afb486d0aa097ffb639127ad
Novak <gnovak.boskov@gmail.com>
2015-07-24 15:48:37 +0200

    Grammar populated with TODOs,project setup added to main script.

--- mean.py
+++ mean.py
@@ -1,9 +1,13 @@
 from textx.metamodel import metamodel_from_file
 from textx.export import metamodel_export, model_export
 
-mean_mm = metamodel_from_file(
-    './lang/mean.tx')
-metamodel_export(mean_mm, 'mean_meta.dot')
+# Project setup
+visualization_dir = "./visualization"
+if not os.path.exists(visualization_dir):
+    os.makedirs(visualization_dir)
+
+mean_mm = metamodel_from_file('./lang/mean.tx')
+metamodel_export(mean_mm, visualization_dir + '/mean_meta.dot')
 
 app = mean_mm.model_from_file('./examples/clue-example.mean')
-model_export(app, 'clue-example.dot')
+model_export(app, visualization_dir + '/clue-example.dot')
