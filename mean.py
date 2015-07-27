b0763013d8946f0cddb0eaf068d03b398b8d9913
Igor Dejanovic <igor.dejanovic@gmail.com>
2015-07-27 15:38:30 +0200

    Fixing imports and paths cross-platform portability.

--- mean.py
+++ mean.py
@@ -1,13 +1,18 @@
+import os
 from textx.metamodel import metamodel_from_file
 from textx.export import metamodel_export, model_export
 
 # Project setup
-visualization_dir = "./visualization"
+VISUALIZATION_DIR = "visualization"
+
+current_dir = os.path.dirname(__file__)
+visualization_dir = os.path.join(current_dir, VISUALIZATION_DIR)
 if not os.path.exists(visualization_dir):
     os.makedirs(visualization_dir)
 
-mean_mm = metamodel_from_file('./lang/mean.tx')
-metamodel_export(mean_mm, visualization_dir + '/mean_meta.dot')
+mean_mm = metamodel_from_file(os.path.join(current_dir, 'lang', 'mean.tx'))
+metamodel_export(mean_mm, os.path.join(visualization_dir, 'mean_meta.dot'))
 
-app = mean_mm.model_from_file('./examples/clue-example.mean')
-model_export(app, visualization_dir + '/clue-example.dot')
+app = mean_mm.model_from_file(os.path.join(current_dir, 'examples',
+                              'clue-example.mean'))
+model_export(app, os.path.join(visualization_dir, 'clue-example.dot'))
